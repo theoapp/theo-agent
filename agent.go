092887efe393463d2576b03b48acc47a6c656f18
Michele Azzolari <michele@azzolari.it>
2018-11-01 12:05:39 +0100

    Split functions in different files

---
+++ agent.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+	"net/http"
+)
+
+func Query(user string, url *string, token *string) int {
+	if url == nil || token == nil {
+		config, ret := parseConfig()
+		if ret > 0 {
+			os.Exit(ret)
+		}
+		body, ret := performQuery(user, config["url"], config["token"])
+		fmt.Println(string(body))
+		if ret == 0 {
+			ret = writeCacheFile(user, body)
+		} else if ret == 20 {
+			ret = retFromFile(user)
+		}
+		
+		os.Exit(ret)
+	} else {
+		_, ret := performQuery(user, *url, *token)
+		return ret;
+	}
+	return 0
+}
+
+func performQuery(user string, url string, token string) ([]byte, int) {
+	
+	remoteUrl := fmt.Sprintf("%s/authorized_keys/%s/%s", url, loadHostname(), user)
+	
+	req, err := http.NewRequest(http.MethodGet, remoteUrl, nil)
+	if err != nil {
+		if *debug {
+			fmt.Fprintf(os.Stderr, "Unable to get remote URL (%s): %s\n", remoteUrl, err)
+		}
+		return nil, 8
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		if *debug {
+			fmt.Fprintf(os.Stderr, "Unable to fetch authorized_keys (%s): %s\n", remoteUrl, err)
+		}
+		return nil, 9
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode > 399 {
+		if *debug {
+			fmt.Fprintf(os.Stderr, "HTTP response error from %s: %d\n", remoteUrl, resp.StatusCode)
+		}
+		return nil, 20
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		if *debug {
+			fmt.Fprintf(os.Stderr, "Unable to parse HTTP response from %s: %s\n", remoteUrl, err)
+		}
+		return nil, 20
+	}
+	return body, 0
+}
+
+func writeCacheFile(user string, body []byte) int {
+	err := ioutil.WriteFile(getUserFilename(user), body, 0644)
+	if err != nil {
+		if *debug {
+			fmt.Fprintf(os.Stderr, "Unable to write cache file (%s): %s\n", getUserFilename(user), err)
+		}
+		return 21
+	}
+	return 0
+}
+
+func getUserFilename(user string ) string {
+	return fmt.Sprintf("%s/%s", *cacheDirPath, user)
+}
+
+func retFromFile(user string) int {
+	dat, err := ioutil.ReadFile(getUserFilename(user))
+    if err != nil {
+		if *debug {
+			fmt.Fprintf(os.Stderr, "Unable to read cache file (%s): %s\n", getUserFilename(user), err)
+		}
+		return 2
+	}
+	fmt.Print(string(dat))
+	return 0
+}
+
+func loadConfig() ([]byte, int) {
+	data, err := ioutil.ReadFile(*configFilePath)
+	if err != nil {
+		if *debug {
+			fmt.Fprintf(os.Stderr, "Unable to read configFile (%s): %s", *configFilePath, err)
+		}
+		return nil, 5
+	}
+	return data, 0
+}
+
+func loadHostname() (hostname string) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		if *debug {
+			fmt.Fprintf(os.Stderr, "Unable to obtain hostname: %s", err)
+		}
+		os.Exit(6)	
+	}
+	return
+}
+
+func parseConfig() (map[string]string, int) {
+	config := make(map[string]string)
+	data, ret := loadConfig()
+	if ret > 0 {
+		return nil, ret
+	}
+	err := yaml.Unmarshal([]byte(data), &config)
+	if err != nil {
+		if *debug {
+			fmt.Fprintf(os.Stderr, "Unable to parse config file (%s): %s\n", *configFilePath, err)
+		}
+		return nil, 7
+	}
+	return config, 0
+}
---
+++ install.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path"
+	"strings"
+	"io/ioutil"
+)
+
+type SshConfig struct {
+	key string
+	value string
+}
+
+var sshconfigs = []SshConfig{
+	SshConfig{"PasswordAuthentication", "no"},
+	SshConfig{"AuthorizedKeysFile", "/var/cache/theo-agent/%%u"},
+	SshConfig{"AuthorizedKeysCommand", "/usr/local/bin/theo-agent"},
+	SshConfig{"AuthorizedKeysCommandUser", "theo-agent"},
+}
+
+func Install() {
+	prepareInstall()
+	checkConfig()
+	mkdirs()
+	writeConfigYaml()
+	if *editSshdConfig {
+		doEditSshdConfig()
+	} else {
+		fmt.Fprintf(os.Stderr, "You didn't specify -sshd-config so you have to edit manually /etc/ssh/sshd_config:\n\n")
+		i := 0
+		for i < len(sshconfigs) {
+			line := fmt.Sprintf("%s %s\n", sshconfigs[i].key, sshconfigs[i].value) // I have to go through fmt.Sprintf because of %%u in sshconfigs[i].value
+			fmt.Fprintf(os.Stderr, line)
+			i++
+		}
+	}
+}
+
+func prepareInstall() {
+
+	askOnce("Theo server URL", theoUrl)
+	if *theoUrl == "" {
+		fmt.Fprintf(os.Stderr, "Missing required Theo URL\n")
+		os.Exit(2)
+	}
+
+	askOnce("Theo access token", theoAccessToken)
+	if *theoAccessToken == "" {
+		fmt.Fprintf(os.Stderr, "Missing required Theo access token\n")
+		os.Exit(2)
+	}
+}
+
+func askOnce(prompt string, result *string) {
+	if *noInteractive {
+		return
+	}
+
+	fmt.Println(prompt)
+
+	if *result != "" {
+		fmt.Printf("[%s]: ", *result)
+	}
+
+	if reader == nil {
+		reader = bufio.NewReader(os.Stdin)
+	}
+
+	data, _, err := reader.ReadLine()
+	if err != nil {
+		panic(err)
+	}
+
+	newResult := string(data)
+	newResult = strings.TrimSpace(newResult)
+
+	if newResult != "" {
+		*result = newResult
+	}
+}
+
+func mkdirs() {
+	dirs := [2]string{ path.Dir(*configFilePath), *cacheDirPath}
+	for i := 0; i < len(dirs); i++ {
+		ensureDir(dirs[i])
+    }
+}
+
+func ensureDir(path string) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err = os.Mkdir(path, 0755)
+		if err != nil {
+			panic(fmt.Sprintf("Unable to create dir (%s): %s", path, err))
+		}
+	}
+}
+
+func checkConfig() {
+	ret := Query("test", theoUrl, theoAccessToken)
+	if ret > 0 {
+		panic(fmt.Sprintf("Check failed, unable to retrieve keys from %s", *theoUrl))
+	}
+}
+
+func writeConfigYaml() {
+	config := fmt.Sprintf("url: %s\ntoken: %s\n", *theoUrl, *theoAccessToken)
+	f, err := os.Create(*configFilePath)
+	if err != nil {
+		if *debug {
+			fmt.Fprintf(os.Stderr, "Unable to write config file (%s): %s", *configFilePath, err)
+		}
+		os.Exit(21)
+	}
+	defer f.Close()
+    
+	_, err = f.WriteString(config)
+	if err != nil {
+		if *debug {
+			fmt.Fprintf(os.Stderr, "Unable to write config file (%s): %s", *configFilePath, err)
+		}
+		os.Exit(21)
+	}
+}
+
+func doEditSshdConfig() bool {
+
+	data, err := ioutil.ReadFile("/etc/ssh/sshd_config")
+	if err != nil {
+		if *debug {
+			fmt.Fprintf(os.Stderr, "Unable to read %s, %s", "/etc/ssh/sshd_config", err)
+		}
+		return false
+	}
+	lines := strings.Split(string(data), "\n")
+	i := 0
+	for i < len(lines) {
+		line := lines[i]
+		ii := 0
+		for ii < len(sshconfigs) {
+			p := strings.Index(line, sshconfigs[ii].key)
+			if p >= 0 {
+				lines[i] = fmt.Sprintf("%s %s", sshconfigs[ii].key, sshconfigs[ii].value)
+				sshconfigs = remove(sshconfigs, ii)
+				break
+			}
+			ii++
+		}
+		i++
+	}
+	ii := 0
+	for ii < len(sshconfigs) {
+		lines = append(lines, fmt.Sprintf("%s %s", sshconfigs[ii].key, sshconfigs[ii].value))
+		ii++
+	}
+
+	f, err := os.Create("/etc/ssh/sshd_config")
+	if err != nil {
+		if *debug {
+			fmt.Fprintf(os.Stderr, "Unable to write config file (%s): %s", "/etc/ssh/sshd_config", err)
+		}
+		os.Exit(21)
+	}
+	defer f.Close()
+    
+	_, err = f.WriteString(strings.Join(lines, "\n"))
+	if err != nil {
+		if *debug {
+			fmt.Fprintf(os.Stderr, "Unable to write config file (%s): %s", "/etc/ssh/sshd_config", err)
+		}
+		os.Exit(21)
+	}
+
+	return true
+}
+
+func remove(s []SshConfig, i int) []SshConfig {
+    s[len(s)-1], s[i] = s[i], s[len(s)-1]
+    return s[:len(s)-1]
+}
