4fca2add5c5f2bcdae60b7ea3e7fbb5d66e70507
Michele Azzolari <michele@azzolari.it>
2020-12-19 15:42:31 +0100

    Remove unused files

--- rename-binaries.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-for file in out/binaries/* ; do 
-  if [ -e "$file" ]; then
-    newname=`echo "$file" | sed 's/amd64/x86_64/; s/386/i686/; s/darwin/Darwin/; s/linux/Linux/; s/freebsd-x86_64/FreeBSD-amd64/; s/arm64/aarch64/;'`
-    mv "$file" "$newname"
-  fi
-done
